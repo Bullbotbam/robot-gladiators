808740487d2f7d2d5329776ff8de89abbdde64cb
BullBot <dbullaustintx@gmail.com>
2021-05-16 22:44:03 -0500

    create js basics for game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("This ia an alert! JavaScript is running!");
+// this creates a function named "fight"
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("Our robot's mame is " + playerName);
+console.log("Tony the Robot" + " is ready for battle!");
+console.log(playerName + "is ready for battle!");
+console.log("Your robot, " + playerName + ", has won!");
+
+function fight() {
+    window.alert("The fight has begun!");
+   
+}
+// fight();
